ec7f5392cd8d3e0810ee42d54c75877c1732b817
thanushpraveenA <praveen.thanush@gmail.com>
2025-08-05 15:13:49 +0530

    Add esbuild and vite dependencies, update package versions, and enhance Vite configuration

--- app/ui/vite.config.ts
+++ app/ui/vite.config.ts
@@ -5,9 +5,17 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   define: {
-    "__APP_VERSION__": JSON.stringify(process.env.npm_package_version),
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
   server: {
-    port: 5123
-  }
+    port: 5123,
+  },
+  optimizeDeps: {
+    include: ["@base-org/account"],
+  },
+  esbuild: {
+    supported: {
+      "import-assertions": true,
+    },
+  },
 });
